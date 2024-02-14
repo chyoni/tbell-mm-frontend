1f360d514ff653b989873c226523a6783e5e909e
최치원 <cw.choiit@choechiwon-ui-MacBookPro.local>
2024-02-14 19:31:55 +0900

    HOST 0.0.0.0

--- docker.build.sh
+++ docker.build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+docker stop tbell-mm-frontend
+
+docker rm -f tbell-mm-frontend
+
 docker build -t tbell-mm-frontend .
 
 docker run -dp 3000:3000 --name tbell-mm-frontend tbell-mm-frontend:latest
