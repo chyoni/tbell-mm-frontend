164301d1a9339e46b33d3cf369561763b367ccba
chyonee <chiwon99881@gmail.com>
2024-02-16 08:53:50 +0900

    favicon

--- src/routes/home/home.tsx
+++ src/routes/home/home.tsx
@@ -1,10 +1,16 @@
 import { Box, Text } from "@chakra-ui/react";
 import React from "react";
+import { Helmet } from "react-helmet-async";
 
 export default function Home() {
   return (
-    <Box>
-      <Text>Dashboard</Text>
-    </Box>
+    <>
+      <Helmet>
+        <title>{`공수율 시스템 대시보드`}</title>
+      </Helmet>
+      <Box>
+        <Text>Dashboard</Text>
+      </Box>
+    </>
   );
 }
