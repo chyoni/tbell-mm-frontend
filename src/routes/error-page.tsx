6f00d8bf5b6e57b405a6d09b2cf10d4bb8cd4794
최치원 <cw.choiit@choechiwon-ui-MacBookPro.local>
2024-02-15 19:32:45 +0900

    context-path

--- src/routes/error-page.tsx
+++ src/routes/error-page.tsx
@@ -1,8 +1,8 @@
-import { Flex, Text } from "@chakra-ui/react";
-import { Helmet } from "react-helmet-async";
-import { Link } from "react-router-dom";
-import { useRouteError } from "react-router-dom";
-import { primaryColor } from "../theme";
+import { Flex, Text } from '@chakra-ui/react';
+import { Helmet } from 'react-helmet-async';
+import { Link } from 'react-router-dom';
+import { useRouteError } from 'react-router-dom';
+import { primaryColor } from '../theme';
 
 export default function ErrorPage() {
   const error = useRouteError();
@@ -14,19 +14,19 @@ export default function ErrorPage() {
         <title>Error Page</title>
       </Helmet>
       <Flex
-        w={"full"}
-        height={"100vh"}
-        justifyContent={"center"}
-        alignItems={"center"}
-        flexDirection={"column"}
+        w={'full'}
+        height={'100vh'}
+        justifyContent={'center'}
+        alignItems={'center'}
+        flexDirection={'column'}
       >
-        <Text fontSize={"xx-large"} fontWeight={600}>
+        <Text fontSize={'xx-large'} fontWeight={600}>
           😢 잘못된 경로입니다.
         </Text>
-        <Text mt={2} fontSize={"large"}>
-          올바르지 않은 경로를 통해 들어온 것 같습니다.
+        <Text mt={2} fontSize={'large'}>
+          올바르지 않은 경로를 통해 들어온 것 같습니다.
         </Text>
-        <Link to={"/"}>
+        <Link to={'/mms'}>
           <Text ml={5} color={primaryColor}>
             메인화면으로 돌아가기 &rarr;
           </Text>
