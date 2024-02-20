c54c1da75ef115ff19a8f948d60d6ee4b9ac2401
chyonee <chiwon99881@gmail.com>
2024-02-20 10:13:19 +0900

    dashboard done

--- src/routes/home/home.tsx
+++ src/routes/home/home.tsx
@@ -1,10 +1,30 @@
-import { Box, Flex, HStack, Skeleton, Text } from '@chakra-ui/react';
-import { useQuery } from '@tanstack/react-query';
-import React, { useEffect, useState } from 'react';
-import { Helmet } from 'react-helmet-async';
-import { IGetStatistics, IGetStatisticsRes } from '../../types/statistics';
-import { IErrorResponse } from '../../types/common';
-import { getHistoryStatistics } from '../../api/statistics';
+import {
+  Box,
+  Button,
+  Flex,
+  HStack,
+  Input,
+  Skeleton,
+  Stat,
+  StatHelpText,
+  StatLabel,
+  StatNumber,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
+import { useQuery } from "@tanstack/react-query";
+import React, { useEffect, useState } from "react";
+import { Helmet } from "react-helmet-async";
+import { IGetStatistics, IGetStatisticsRes } from "../../types/statistics";
+import { IErrorResponse } from "../../types/common";
+import { getHistoryStatistics } from "../../api/statistics";
+import { primaryColor, titleColor } from "../../theme";
+import { getKoreaDateTime } from "../../utils";
+import { NumericFormat } from "react-number-format";
+import { IGetEmployees } from "../../types/employee";
+import { getEmployees } from "../../api/employees";
+import { IGetProjects } from "../../types/project";
+import { getProjects } from "../../api/projects";
 
 export default function Home() {
   const [year, setYear] = useState<string>(new Date().getFullYear().toString());
@@ -16,9 +36,37 @@ export default function Home() {
     IGetStatisticsRes,
     IErrorResponse
   >({
-    queryKey: ['statistics'],
+    queryKey: ["statistics"],
     queryFn: () => getHistoryStatistics(year),
+    refetchOnWindowFocus: false,
   });
+
+  const { isLoading: employeesLoading, data: employeesData } = useQuery<
+    IGetEmployees,
+    IErrorResponse
+  >({
+    queryKey: ["employees"],
+    queryFn: () => getEmployees(0, 10),
+    refetchOnWindowFocus: false,
+  });
+
+  const { isLoading: projectsLoading, data: projectsData } = useQuery<
+    IGetProjects,
+    Error
+  >({
+    queryKey: ["projects"],
+    queryFn: () => getProjects(0, 10),
+    refetchOnWindowFocus: false,
+  });
+
+  const handleKeyUp = (
+    e: React.KeyboardEvent<HTMLButtonElement | HTMLInputElement>
+  ) => {
+    if (e.key === "Enter") {
+      searchByCond();
+    }
+  };
+  const searchByCond = async () => await refetch();
 
   useEffect(() => {
     if (data?.ok && data.data) {
@@ -52,71 +100,166 @@ export default function Home() {
       <Helmet>
         <title>{`공수율 시스템 대시보드`}</title>
       </Helmet>
-      <Text fontSize={'2xl'} fontWeight={'semibold'}>
+      <Text fontSize={"2xl"} fontWeight={"semibold"}>
         공수율 관리 시스템 대시보드
       </Text>
-      <Skeleton isLoaded={!isLoading}>
-        <Box
-          w={'full'}
-          h={'50vh'}
+      <Skeleton isLoaded={!isLoading} borderRadius={20}>
+        <VStack
+          h={"max-content"}
           marginTop={8}
-          p={10}
-          border={'ButtonFace'}
-          borderStyle={'dashed'}
+          px={10}
+          py={5}
+          border={"ButtonFace"}
+          borderStyle={"dashed"}
           borderRadius={20}
-          borderWidth={1}
+          borderWidth={2}
+          direction={"column"}
         >
+          <HStack justifyContent={"space-between"} marginBottom={3}>
+            <Text fontWeight={"semibold"} fontSize={"sm"}>
+              전체 공수율 현황
+            </Text>
+            <Text fontSize={"sm"} color={"#C6C7C9"}>
+              {getKoreaDateTime()} 기준
+            </Text>
+          </HStack>
+
+          {/* 검색 섹션 */}
+          <HStack marginBottom={5} spacing={8}>
+            <Box width={"min-content"} alignItems={"center"} display={"flex"}>
+              <Text
+                marginRight={2}
+                fontWeight={"hairline"}
+                width={"max-content"}
+              >
+                연도
+              </Text>
+              <Input
+                onChange={(event) => setYear(event.target.value)}
+                placeholder={new Date().getFullYear().toString()}
+                size="sm"
+                type="text"
+                value={year}
+                width={110}
+                focusBorderColor={primaryColor}
+                onKeyUp={handleKeyUp}
+              />
+            </Box>
+            <Button
+              colorScheme="teal"
+              size={"sm"}
+              onClick={searchByCond}
+              onKeyUp={handleKeyUp}
+            >
+              검색
+            </Button>
+          </HStack>
+          {/* 검색 섹션 끝 */}
+
+          {/* 데이터 */}
           <HStack>
-            <Flex direction={'column'} marginRight={10}>
-              <Flex alignItems={'center'} justifyContent={'center'}>
-                <Text>전체</Text>
+            <Flex direction={"column"} marginRight={10}>
+              <Flex alignItems={"center"} justifyContent={"center"}>
+                <Text fontWeight={"bold"} fontSize={"large"}>
+                  구분
+                </Text>
               </Flex>
               <Box>
-                <Text>투입MM</Text>
+                <Text color={titleColor}>투입MM</Text>
               </Box>
               <Box>
-                <Text>투입금액</Text>
+                <Text color={titleColor}>투입금액</Text>
               </Box>
               <Box>
-                <Text>정산MM</Text>
+                <Text color={titleColor}>정산MM</Text>
               </Box>
               <Box>
-                <Text>정산금액</Text>
+                <Text color={titleColor}>정산금액</Text>
               </Box>
             </Flex>
             {statisticsByYear.map((s, index) => (
-              <Flex direction={'column'} key={index}>
-                <Flex alignItems={'center'} justifyContent={'center'}>
-                  <Text>{`${s.month}월`}</Text>
+              <Flex direction={"column"} key={index} marginRight={6}>
+                <Flex alignItems={"center"} justifyContent={"center"}>
+                  <Text
+                    fontWeight={"bold"}
+                    fontSize={"large"}
+                  >{`${s.month}월`}</Text>
                 </Flex>
-                <Flex alignItems={'center'} justifyContent={'center'}>
-                  <Text>
-                    {s.totalInputManMonth !== null ? s.totalInputManMonth : '-'}
+                <Flex alignItems={"center"} justifyContent={"center"}>
+                  <Text fontWeight={"hairline"}>
+                    {s.totalInputManMonth !== null ? s.totalInputManMonth : "-"}
                   </Text>
                 </Flex>
-                <Flex alignItems={'center'} justifyContent={'center'}>
-                  <Text>
-                    {s.totalInputPrice !== null ? s.totalInputPrice : '-'}
-                  </Text>
+                <Flex alignItems={"center"} justifyContent={"center"}>
+                  <NumericFormat
+                    value={s.totalInputPrice !== null ? s.totalInputPrice : "-"}
+                    displayType="text"
+                    thousandSeparator={","}
+                    className="text-md font-thin"
+                  />
                 </Flex>
-                <Flex alignItems={'center'} justifyContent={'center'}>
-                  <Text>
+                <Flex alignItems={"center"} justifyContent={"center"}>
+                  <Text fontWeight={"hairline"}>
                     {s.totalCalculateManMonth !== null
                       ? s.totalCalculateManMonth
-                      : '-'}
+                      : "-"}
                   </Text>
                 </Flex>
-                <Flex alignItems={'center'} justifyContent={'center'}>
-                  <Text>
-                    {s.totalCalculatePrice !== null
-                      ? s.totalCalculatePrice
-                      : '-'}
-                  </Text>
+                <Flex alignItems={"center"} justifyContent={"center"}>
+                  <NumericFormat
+                    value={
+                      s.totalCalculatePrice !== null
+                        ? s.totalCalculatePrice
+                        : "-"
+                    }
+                    displayType="text"
+                    thousandSeparator={","}
+                    className="text-md font-thin"
+                  />
                 </Flex>
               </Flex>
             ))}
           </HStack>
-        </Box>
+          {/* 데이터 끝 */}
+        </VStack>
+      </Skeleton>
+      <Skeleton
+        isLoaded={!employeesLoading && !projectsLoading}
+        borderRadius={20}
+      >
+        <HStack marginTop={5}>
+          <Stat
+            borderWidth={2}
+            borderStyle={"dashed"}
+            padding={2}
+            borderRadius={10}
+            width={"min-content"}
+          >
+            <StatLabel>사원 수</StatLabel>
+            <StatNumber fontSize={"3xl"}>
+              {employeesData?.ok
+                ? `${employeesData?.data.totalElements} 명`
+                : "데이터를 불러오지 못했습니다."}
+            </StatNumber>
+            <StatHelpText>{getKoreaDateTime()} 기준</StatHelpText>
+          </Stat>
+
+          <Stat
+            borderWidth={2}
+            borderStyle={"dashed"}
+            padding={2}
+            borderRadius={10}
+            width={"min-content"}
+          >
+            <StatLabel>프로젝트 수</StatLabel>
+            <StatNumber fontSize={"3xl"}>
+              {projectsData?.ok
+                ? `${projectsData.data.totalElements} 개`
+                : "데이터를 불러오지 못했습니다."}
+            </StatNumber>
+            <StatHelpText>{getKoreaDateTime()} 기준</StatHelpText>
+          </Stat>
+        </HStack>
       </Skeleton>
     </>
   );
--- src/utils.ts
+++ src/utils.ts
@@ -1,20 +1,20 @@
 export const unitPriceLv = [
-  { value: 'BEGINNER', label: '초급' },
-  { value: 'HIGH_BEGINNER', label: '초급(상)' },
-  { value: 'MEDIUM_BEGINNER', label: '초급(중)' },
-  { value: 'LOW_BEGINNER', label: '초급(하)' },
-  { value: 'INTERMEDIATE', label: '중급' },
-  { value: 'HIGH_INTERMEDIATE', label: '중급(상)' },
-  { value: 'MEDIUM_INTERMEDIATE', label: '중급(중)' },
-  { value: 'LOW_INTERMEDIATE', label: '중급(하)' },
-  { value: 'ADVANCED', label: '고급' },
-  { value: 'HIGH_ADVANCED', label: '고급(상)' },
-  { value: 'MEDIUM_ADVANCED', label: '고급(중)' },
-  { value: 'LOW_ADVANCED', label: '고급(하)' },
-  { value: 'EXPERT', label: '특급' },
-  { value: 'HIGH_EXPERT', label: '특급(상)' },
-  { value: 'MEDIUM_EXPERT', label: '특급(중)' },
-  { value: 'LOW_EXPERT', label: '특급(하)' },
+  { value: "BEGINNER", label: "초급" },
+  { value: "HIGH_BEGINNER", label: "초급(상)" },
+  { value: "MEDIUM_BEGINNER", label: "초급(중)" },
+  { value: "LOW_BEGINNER", label: "초급(하)" },
+  { value: "INTERMEDIATE", label: "중급" },
+  { value: "HIGH_INTERMEDIATE", label: "중급(상)" },
+  { value: "MEDIUM_INTERMEDIATE", label: "중급(중)" },
+  { value: "LOW_INTERMEDIATE", label: "중급(하)" },
+  { value: "ADVANCED", label: "고급" },
+  { value: "HIGH_ADVANCED", label: "고급(상)" },
+  { value: "MEDIUM_ADVANCED", label: "고급(중)" },
+  { value: "LOW_ADVANCED", label: "고급(하)" },
+  { value: "EXPERT", label: "특급" },
+  { value: "HIGH_EXPERT", label: "특급(상)" },
+  { value: "MEDIUM_EXPERT", label: "특급(중)" },
+  { value: "LOW_EXPERT", label: "특급(하)" },
 ];
 
 export const convertLevelEnToKo = (level: string): string => {
@@ -24,3 +24,19 @@ export const convertLevelEnToKo = (level: string): string => {
 
   return koLabel.label;
 };
+
+export function getKoreaDateTime(): string {
+  const koreaTimeZone = "Asia/Seoul"; // Time zone for Korea Standard Time
+  const options: Intl.DateTimeFormatOptions = {
+    timeZone: koreaTimeZone,
+    hour12: false, // Use 24-hour format
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+  };
+
+  return new Date().toLocaleString("en-US", options);
+}
